fb296459945c71bc6a6b6feabb5a73935fec07e4
jennaram <63740974+jennaram@users.noreply.github.com>
2025-02-07 10:54:45 +0100

    header

---
+++ header.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TechPulse</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="index.php">TechPulse</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="promos.php">Promos</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Catégories
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="smartphones.php">Smartphones</a>
+                        <a class="dropdown-item" href="ordinateurs.php">Ordinateurs portables</a>
+                        <a class="dropdown-item" href="tablettes.php">Tablettes</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="about.php">Qui sommes-nous ?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contactez-nous</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <script src="js/jquery-3.5.1.slim.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,6 @@
+<?php include 'header.php'; ?>
+<div class="container">
+    <h1>Bienvenue sur TechPulse</h1>
+    <p>Découvrez nos dernières offres sur les smartphones, ordinateurs portables et tablettes.</p>
+</div>
+<?php include 'footer.php'; ?>
